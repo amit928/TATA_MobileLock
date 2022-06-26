50408e571258b84f4d593f78ce94624a9f0eb757
amit kumar mallick <amitzqx@gmail.com>
2022-06-26 12:24:55 +0530

    New Apk created

---
+++ App2.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App2() {
+  return (
+    <View >
+      <Text>This is app.js page!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
